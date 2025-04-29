5aefc58b9bcaf18ea591eebd80d2d093a5cf0682
Steven Sourbron <s.sourbron@sheffield.ac.uk>
2025-04-29 09:52:29 +0100

    bump version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@
 project = 'pydmr'
 copyright = '2025, pydmr contributors'
 author = 'pydmr contributors'
-release = '0.0.0'
+release = '0.0.1'
 
 # -- Path setup --------------------------------------------------------------
 # If extensions (or modules to document with autodoc) are in another directory,
